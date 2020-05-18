94e677773ca20cd67e60e36f4fa8462c6e745547
Kirinator <l.k.1910@mail.ru>
2020-05-18 03:53:29 +0300

    Завершил метод Process

--- KTP-7/src/Crawler.java
+++ KTP-7/src/Crawler.java
@@ -4,36 +4,16 @@ import java.util.LinkedList;
 
 public class Crawler {
 
-    LinkedList <URLDepthPair> findLink = new LinkedList <URLDepthPair>();
-    LinkedList <URLDepthPair> viewedLink = new LinkedList <URLDepthPair>();
+    static LinkedList <URLDepthPair> findLink = new LinkedList <URLDepthPair>();
+    static LinkedList <URLDepthPair> viewedLink = new LinkedList <URLDepthPair>();
 
-//    public void getSites() {
-//        for (var elem : m_Processed)
-//            System.out.println(elem.getURL());
-//        System.out.println("Links visited: " + m_Processed.size());
-//    }
 
-//    public static boolean check(LinkedList<URLDepthPair> viewedLink,URLDepthPair pair)
-//    {
-//        boolean isAlready = true;
-//        for (URLDepthPair c : viewedLink)
-//            if (c.toString().equals(pair.toString()))
-//                isAlready=false;
-//        return isAlready;
-//    }
-
-    public static void getSites(LinkedList<URLDepthPair> viewedLink){
+    public static void showResult(LinkedList<URLDepthPair> viewedLink)
+    {
         for (URLDepthPair c : viewedLink)
             System.out.println("Depth : "+c.getDepth() + "\tLink : "+c.toString());
     }
 
-    public void getInputStream(Socket my_socket) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(my_socket.getInputStream()));
-    }
-
-    public void getOutputStream(Socket my_socket) throws IOException {
-        PrintWriter out = new PrintWriter (my_socket.getOutputStream(),true);
-    }
 
     public static void request(PrintWriter out,URLDepthPair pair) throws MalformedURLException {
         out.println("GET " + pair.getPath() + " HTTP/1.1");
@@ -43,8 +23,8 @@ public class Crawler {
         out.flush();
     }
 
-    public void Process(URLDepthPair pair,int maxDepth) throws IOException {
-        findLink.add(pair);
+    public static void Process(String pair, int maxDepth) throws IOException {
+        findLink.add(new URLDepthPair(pair,0));
         while (!findLink.isEmpty()){
             URLDepthPair currentPair = findLink.removeFirst();
             Socket my_socket =new Socket(currentPair.getHost(), 80);
@@ -52,41 +32,35 @@ public class Crawler {
             BufferedReader in = new BufferedReader(new InputStreamReader(my_socket.getInputStream()));
             PrintWriter out = new PrintWriter (my_socket.getOutputStream(),true);
             request(out, currentPair);
-            String line=in.readLine();
-            while(line!=null){
+            String line;
+            while((line=in.readLine())!=null){
                 if(currentPair.test(line,maxDepth)){
                     StringBuilder currentLink = new StringBuilder();
-                    char c = line.charAt(line.indexOf(currentPair.URL_PREFIX)+8);
-//                    currentLink.append(c);
-                    for (int i = line.indexOf(currentPair.URL_PREFIX) + 9; c != '"'; i++) {
-//                        c = line.charAt(i);
-//                        if (c == '"')
-//                            isLinkFound = true;
-//                        else
+                    for (int i = line.indexOf(currentPair.URL_PREFIX) + 9; line.charAt(i) != '"'; i++) {
                             currentLink.append(line.charAt(i));
                     }
-//                    if (isLinkFound) {
-                        currentPair.depth+=1;
-                        currentPair.URL=currentLink.toString();
-
-//                        URLDepthPair newPair = new URLDepthPair(currentLink.toString(), (currentPair.depth + 1));
-                        if (currentPair.check(findLink, currentPair)) {
-                            findLink.add(currentPair);
-//                        }
-                    }
-
+                    URLDepthPair newPair = new URLDepthPair(currentLink.toString(), currentPair.depth + 1);
+                    if (currentPair.check(findLink, newPair) && currentPair.check(viewedLink, newPair))
+                        findLink.add(newPair);
 
                 }
-
             }
-
+            my_socket.close();
+            viewedLink.add(currentPair);
         }
-
-
-
+        showResult(viewedLink);
     }
 
+    public static void main(String[] args) {
 
+
+        String[] arg = new String[]{"http://government.ru/","1"};
+            try {
+                Process(arg[0], Integer.parseInt(arg[1]));
+            } catch (NumberFormatException | IOException e) {
+                System.out.println("usage: java Crawler <URL><depth>");
+            }
+    }
 
 
 }
