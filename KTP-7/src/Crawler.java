362b0e3298ffe8a4bc8a27592fa1cb7cc139714f
Kirinator <l.k.1910@mail.ru>
2020-05-18 12:06:54 +0300

    Завершил метод Process

--- KTP-7/src/Crawler.java
+++ KTP-7/src/Crawler.java
@@ -60,7 +60,7 @@ public class Crawler {
     public static void main(String[] args) {
 
 
-        String[] arg = new String[]{"http://government.ru/","2"};
+        String[] arg = new String[]{"http://government.ru/","1"};
             try {
                 try {
                     Process(arg[0], Integer.parseInt(arg[1]));
