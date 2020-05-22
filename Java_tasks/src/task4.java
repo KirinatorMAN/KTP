5b09978c9b40f102871a8940fc71d811a5ac4793
Kirinator <l.k.1910@mail.ru>
2020-05-22 16:31:32 +0300

    Loaded task4 with others functions

--- Java_tasks/src/task4.java
+++ Java_tasks/src/task4.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class task4 {
 
@@ -18,7 +15,7 @@ public class task4 {
                 s+=essay[i]+" ";
                 b+=essay[i].length();
             }
-            else {
+            else{
                 s += "\n" + essay[i]+" ";
                 b=essay[i].length();
             }
@@ -48,101 +45,145 @@ public class task4 {
         return res;
     }
 
-    public static boolean checkPerfect(int a){
-        int result=0;
-        for (int i=a-1; i>0; i--){
-            if (a%i==0) result+=i;
-        }
-        return result==a;
-    }
-
-    public static String flipEndChars(String a){
-//        a.charAt(a.length()-1)==b.charAt(0) && b.charAt(b.length()-1)==a.charAt(0)
-        if(a.length()<2) return"Incompatible.";
-        if(a.charAt(a.length()-1)==a.charAt(0)) return "Two's a pair.";
-        return a.charAt(a.length()-1)+a.substring(1,a.length()-1)+a.charAt(0);
-    }
-
-    public static boolean isValidHexCode(String a){
-        Pattern p =Pattern.compile("(^[#][a-y[A-Y][0-9]]{6})");
-        Matcher m = p.matcher(a);
-        return m.matches();
-    }
-
-    public static boolean same(int[] arr1, int[] arr2){
-        int uniqueValue=0;
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        int maxLength = Math.max(arr1.length - 1, arr2.length - 1);
-        for (int i = 0; i<maxLength;i++){
-            if (i<arr1.length-1)
-                if (Arrays.binarySearch(arr1,i+1,arr1.length-1,arr1[i])<0)
-                    uniqueValue++;
-            if (i<arr2.length-1)
-                if (Arrays.binarySearch(arr2,i+1,arr2.length-1,arr2[i])<0)
-                    uniqueValue--;
-        }
-        uniqueValue += arr1.length==1 ? 1 : arr1[arr1.length-2]!=arr1[arr1.length-1] ? 1:0;//checks last element
-        uniqueValue -= arr2.length==1 ? 1 : arr2[arr2.length-2]!=arr2[arr2.length-1] ? 1:0;
-        return uniqueValue==0;
-    }
-
-    public static boolean isKaprekar(int a){
-        String s1 = Integer.toString(a * a).substring(0, Integer.toString(a * a).length() / 2);
-        int a2 = Integer.parseInt(Integer.toString(a * a).substring(Integer.toString(a * a).length() / 2));
-
-        if (s1.length() == 0)
-            return a2 ==a;
-        else
-            return Integer.parseInt(s1)+ a2 ==a;
-    }
-
-    public static String longestZero(String a){
-        StringBuilder result=new StringBuilder();
-        while (a.indexOf(result+"0")!=-1)
-            result.append("0");
+    public static String toCamelCase(String camel){
+        String[] massiveOfSnakeCase = camel.split("_");
+        StringBuilder result= new StringBuilder(massiveOfSnakeCase[0]);
+        for (String cWord:massiveOfSnakeCase)
+            if (!result.toString().equals(cWord))
+                result.append(Character.toUpperCase(cWord.charAt(0))).append(cWord.substring(1));
         return result.toString();
     }
 
-    public static int nextPrime(int a){
-        for(int i=a;true;){
-            for(int j=2;j<i;j++){
-                if(i%j==0) {
-                    i++;
-                    j = 2;
-                }
-            }
-            return i;
+    public static String toSnakeCase(String snake){
+        String result="";
+        for (char cChar:snake.toCharArray()) {
+            if ((cChar!= ' ')&&(cChar!= '\t')&&(cChar!= '\n'))
+                if (Character.isLetter(cChar))
+                    if (Character.isUpperCase(cChar))
+                        result+="_"+Character.toLowerCase(cChar);
+                    else
+                        result+=cChar;
+                else
+                    result+=cChar;
         }
+        return result;
     }
 
-    public static boolean rightTriangle(int x, int y, int z){
-        int max = Math.max(x,Math.max(y,z));
-        int m1=0;
-        int m2=0;
-        if (max==x)
-            m1=y;
-        else if (max==y)
-            m1=z;
+    public static String overTime(double[] a){
+        double res= 0;
+        if(a[0]<=17){
+            if (a[1]>17)
+                res+=(17-a[0])*a[2]+(a[1]-17)*a[2]*a[3];
+            else
+                res+=(a[1]-a[0])*a[2];
+        }
         else
-            m1=x;
-        m2=x+y+z-max-m1;
-        return max*max==m1*m1+m2*m2;
+            res+=(a[1]-a[0])*a[2]*a[3];
+        return "$"+String.format("%.2f", res);
     }
 
+    public static String BMI(String w, String h){
+        String[] we=w.split(" ");
+        String[] hi=h.split(" ");
+        double k=0;
+        double m=0;
+        if (we[1].equals("pounds"))
+            k=Double.parseDouble(we[0])*0.45;
+        else
+            k=Double.parseDouble(we[0]);
+        if (hi[1].equals("inches"))
+            m=Double.parseDouble(hi[0])*0.025;
+        else
+            m=Double.parseDouble(hi[0]);
+        double res=k/(m*m);
+        if (res<18.5)
+            return String.format("%.1f",res)+" Underweight";
+        if (res>=25)
+            return String.format("%.1f",res)+" Overweight";
+        return String.format("%.1f",res)+" Normal weight";
+    }
 
+    public static int  bugger(int a){
+        int count = 0;
+        while(a>=10){
+            int rValue = 1;
+            for (char cChar:String.valueOf(a).toCharArray())
+                rValue*= Integer.parseInt(String.valueOf(cChar));
+            a=rValue;
+            count++;
+        }
+        return  count;
+    }
+
+    public static String toStarShorthand(String a){
+        int count = 1;
+        String result = "";
+        for (int i = 0; i<a.length()-1;i++){
+            if(a.charAt(i)==a.charAt(i+1))
+                count++;
+            else{
+                result += count == 1 ? a.charAt(i) :a.charAt(i) + "*" + count;
+                count=1;
+            }
+        }
+        if (a.length()!=0)
+            result+=count!=1 ?a.charAt(a.length()-1)+"*"+count : a.charAt(a.length()-1);
+        return  result;
+    }
+
+    public static boolean  doesRhyme(String str1, String str2){
+        String[] words1 = str1.split(" ");
+        String[] words2 = str2.split(" ");
+
+        String vowels = "AEIYUOaeiyuo";
+        String result1 = "";
+        String result2 = "";
+        for (int i = 0; i < words1[words1.length - 1].length(); i++){
+            if (vowels.contains(Character.toString(words1[words1.length - 1].charAt(i))))
+                result1 += words1[words1.length - 1].charAt(i);
+        }
+        for (int i = 0; i < words2[words2.length - 1].length(); i++){
+            if (vowels.contains(Character.toString(words2[words2.length - 1].charAt(i))))
+                result2 += words2[words2.length - 1].charAt(i);
+        }
+        return result1.toLowerCase().equals(result2.toLowerCase());
+    }
+
+    public static boolean trouble(long a, long b){
+        for (int i = 1; i < 10; i++){
+            if (Long.toString(a).contains(Integer.toString(i) + i + i)){
+                if (Long.toString(b).contains(Integer.toString(i) + i))
+                    return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static int countUniqueBooks(String text, char end){
+        String  uniquebooks = "";
+        while ((text.indexOf(end)!=-1)&&(text.indexOf(end,text.indexOf(end)+1)!=-1)){
+            String tempLine = text.substring(text.indexOf(end)+1,text.indexOf(end,text.indexOf(end)+1));
+            text=text.substring(text.indexOf(end,text.indexOf(end)+1)+1);
+            for (char cChar:tempLine.toCharArray()){
+                uniquebooks += uniquebooks.indexOf(cChar)==-1 ? cChar : "";
+            }
+        }
+        return uniquebooks.length();
+    }
 
     public static void main(String[] args) {
         System.out.println(textEditor(10,7, "hello my name is Bessie and this is my essay"));
-        System.out.println(split("((())())(()(()()))"));
-        System.out.println(checkPerfect(6));
-        System.out.println(flipEndChars("Cat, dog, and mouse."));
-        System.out.println(isValidHexCode("#CD5C5C"));
-        System.out.println(same(new int[] {1, 3, 4, 4, 4},new int[] {2, 5, 7}));
-        System.out.println(isKaprekar(3));
-        System.out.println(longestZero("01100001011000"));
-        System.out.println(nextPrime(12));
-        System.out.println(rightTriangle(3, 4, 5));
+        System.out.println(split("()()()"));
+        System.out.println(toCamelCase("hello_edabit"));
+        System.out.println(toSnakeCase("helloEdabit"));
+        System.out.println(overTime(new double[] {9, 17, 30, 1.5}));
+        System.out.println(BMI("205 pounds", "73 inches"));
+        System.out.println(bugger(39));
+        System.out.println(toStarShorthand("abbccc"));
+        System.out.println(doesRhyme("Sam I am!", "Green eggs and ham."));
+        System.out.println(trouble(666789, 12345667));
+        System.out.println(countUniqueBooks("AZYWABBCATTTA", 'A'));
 
     }
 }
